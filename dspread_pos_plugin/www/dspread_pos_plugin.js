499b5016d627a96c78944f3581bb4299789dc8c4
believemeng <945858825@qq.com>
2018-02-02 19:12:43 +0800

    add new api

--- dspread_pos_plugin/www/dspread_pos_plugin.js
+++ dspread_pos_plugin/www/dspread_pos_plugin.js
@@ -1,60 +1,53 @@
-cordova.define("posPlugin.dspread_pos_plugin", function(require, exports, module) {
-cordova.define("posPlugin.dspread_pos_plugin", function(require, exports, module) {
 var exec = require('cordova/exec');
 
-var posPlug =function(){};
-posPlug.prototype.scanQPos2Mode=function(){
-  exec(success,fail,"dspread_pos_plugin","scanQPos2Mode",[]);
-}
+var posPlug = {
+	  scanQPos2Mode:function(success,fail){
+	   exec(success,fail,"dspread_pos_plugin","scanQPos2Mode",[]);
+	},
+	
+	  connectBluetoothDevice:function(success,fail,isConnect){
+		  exec(success,fail,"dspread_pos_plugin","connectBluetoothDevice",[isConnect]);
+	},
 
-posPlug.prototype.connectBluetoothDevice=function(isConnect){
-  exec(success,fail,"dspread_pos_plugin","connectBluetoothDevice",[isConnect]);
-}
-
-posPlug.prototype.doTrade=function(timeout){
-	  exec(success,faill,"dspread_pos_plugin","doTrade",[timeout]);
-	}
-
-posPlug.prototype.getDeviceList=function(){
-	  exec(success,fail,"dspread_pos_plugin","getDeviceList",[]);
-	}
-
-posPlug.prototype.stopScanQPos2Mode=function(){
-	  exec(success,fail,"dspread_pos_plugin","stopScanQPos2Mode",[]);
-	}
-
-posPlug.prototype.disconnectBT=function(){
-	  exec(success,fail,"dspread_pos_plugin","disconnectBT",[]);
-	}
-
-posPlug.prototype.getQposInfo=function(){
-	  exec(success,fail,"dspread_pos_plugin","getQposInfo",[]);
-	}
-
-posPlug.prototype.getQposId=function(){
-	  exec(success,fail,"dspread_pos_plugin","getQposId",[]);
-	}
-
-posPlug.prototype.updateIPEK=function(){
-	  exec(success,fail,"dspread_pos_plugin","updateIPEK",[]);
-	}
-
-posPlug.prototype.updateEmvApp=function(){
-	  exec(success,fail,"dspread_pos_plugin","updateEmvApp",[]);
-	}
-
-posPlug.prototype.updateEmvCAPK=function(){
-	  exec(success,fail,"dspread_pos_plugin","updateEmvCAPK",[]);
-	}
-
-posPlug.prototype.setMasterKey=function(){
-	  exec(success,fail,"dspread_pos_plugin","setMasterKey",[key,checkValue]);
-	}
-
-
-var  pos=new posPlug();
-module.exports=pos;
-
-});
-
-});
+	doTrade:function(success,faill,timeout){
+		  exec(success,faill,"dspread_pos_plugin","doTrade",[timeout]);
+		},
+	
+	getDeviceList:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","getDeviceList",[]);
+		},
+	
+	stopScanQPos2Mode:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","stopScanQPos2Mode",[]);
+		},
+	
+	disconnectBT:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","disconnectBT",[]);
+		},
+	
+	getQposInfo:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","getQposInfo",[]);
+		},
+	
+	getQposId:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","getQposId",[]);
+		},
+	
+	updateIPEK:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","updateIPEK",[]);
+		},
+	
+	updateEmvApp:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","updateEmvApp",[]);
+		},
+	
+	updateEmvCAPK:function(success,fail){
+		  exec(success,fail,"dspread_pos_plugin","updateEmvCAPK",[]);
+		},
+	
+	setMasterKey:function(success,fail,key,checkValue){
+		  exec(success,fail,"dspread_pos_plugin","setMasterKey",[key,checkValue]);
+		}
+	
+	};
+	module.exports =posPlug;
